0cfa5712420685f7ba3b8126bb71acdad470174c
David Scott <sirdavidscott@hotmail.com>
2022-11-28 17:07:04 +0000

    14th commit add extra student info

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  # creates an empty array
-  students = []
-  # get the first name
-  name = gets.chomp 
-  # while the name is NOT empty repeat this code
-  while !name.empty? do
-    students <<{name: name, cohort: :november} # for each name entered, creates a template for the hash
-    puts "Now we have #{students.count} students"
-    # get another name from user 
     name = gets.chomp
+    students = []
+    while !name.empty? do
+      puts "Please enter the hobby of the student"
+      hobby = gets.chomp 
+      puts "Please enter the student's country of birth"
+      country = gets.chomp 
+      puts "Please enter the height of the student"
+      height = gets.chomp 
+      students <<{name: name, hobbies: hobby, country_of_birth: country, height: height, cohort: :november} # for each name entered, creates a template for the hash
+      puts "Now we have #{students.count} students"
+      puts "Please enter the name of the student"
+      # get another name from user 
+      name = gets.chomp
+    end
+    #returns the array of students
+    students
   end
-  #returns the array of students
-  students
-end
 
 def print_header
   puts "The students of Villains Academy"
@@ -26,19 +30,11 @@ def print(students)
   while print_count <= 1 do
     print_count += 1
     students.each.with_index(1) do |student, i|
-      puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{i}. #{student[:name]}, Hobby: #{student[:hobbies]}, Country of Birth: #{student[:country_of_birth]}, Height: #{student[:height]}, (#{student[:cohort]} cohort)"
+      # puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
-
-# def print(students)
-#   while true do
-#     students.each_with_index(1) do |student, i|
-#       puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
-#   end
-# end
-# end
-
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
