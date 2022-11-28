742e9829f5e214f6b8dee99fad3e80f560d36dfc
David Scott <sirdavidscott@hotmail.com>
2022-11-28 12:07:47 +0000

    9th commit interactive method creation

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-#let's put all the students into an array
-students = [ 
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Witch of the West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Krueger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name:"Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # creates an empty array
+  students = []
+  # get the first name
+  name = gets.chomp 
+  # while the name is NOT empty repeat this code
+  while !name.empty? do
+    students <<{name: name, cohort: :november} # for each name entered, creates a template for the hash
+    puts "Now we have #{students.count} students"
+    # get another name from user 
+    name = gets.chomp
+  end
+  #returns the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,7 +31,13 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 #nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
+
+
+
+
+
