3c01499fc5ffb39271c47a6327242f7573e1b8d5
David Scott <sirdavidscott@hotmail.com>
2022-12-02 13:25:26 +0000

    23rd commit load csv

--- directory.rb
+++ directory.rb
@@ -16,6 +16,8 @@ def process(selection)
       show_students
    when "3"
     save_students
+   when "4"
+    load_students
    when "9"
       exit
    else
@@ -96,6 +98,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -109,5 +112,13 @@ end
 file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 
 interactive_menu
