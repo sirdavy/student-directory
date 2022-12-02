07f20ea4fd5c8329eb2b2bb6f07002a7466ac937
David Scott <sirdavidscott@hotmail.com>
2022-12-02 14:25:59 +0000

    24rd commit load csv from CL

--- directory.rb
+++ directory.rb
@@ -1,17 +1,24 @@
 @students = []
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def process(selection)
   case selection
     when "1"
-      students = input_students
+      input_students
    when "2"
       show_students
    when "3"
@@ -29,11 +36,11 @@ end
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-    name = gets.delete("\n")   #.chomp.capitalize
+    name = STDIN.gets.delete("\n")   #.chomp.capitalize
     cohort_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     while !name.empty? do
       puts "Please enter cohort"
-      cohort = gets.chomp.capitalize
+      cohort = STDIN.gets.chomp.capitalize
       # puts "Please enter the hobby of the student"
       # hobby = gets.chomp 
       # puts "Please enter the student's country of birth"
@@ -48,59 +55,52 @@ def input_students
       # puts "Now we have #{students.count} students"
       puts "Please enter the name of the student"
       # get another name from user 
-      name = gets.chomp
+      name = STDIN.gets.chomp
     end
     #returns the array of students
     # students #think this can go
   end
 
-def print_students_list
-  print_count = 1
-  while print_count <= 1 do
-    print_count += 1
-    cohorts = @students.map { |p| p[:cohort] }.uniq
-    #maps the "students" array - each element a hash -, designates each hash 'p', 
-    #takes each hash and calls the value of the cohort key 
-    # .uniqs them so there aren't doubles and puts them in an array called "cohorts". So an order is set.
-    cohorts.each do |x|
-      # goes through the new array "cohorts" and designates each month as X  
-    temp_students = @students.select { |p| p[:cohort] == x}
-    # creates a new array "temp_students" then selects to go in it from 
-    #the "students" array the hashes where cohort equals eg november, december then jan as per order from .uniq
-    temp_students.each do |student|
-      #temp_students is now an array filled with elements that are hashes 
-      #in order of month. this designates each hash as "student" 
-    # puts "#{student[:name]}".ljust(20) + "Hobby: #{student[:hobbies]}".ljust(25) + "Country of Birth: #{student[:country_of_birth]}".ljust(35) + "Height: #{student[:height]}".ljust(25) + "(#{student[:cohort]} cohort)".ljust(20)
-  #then says print each "student" with these keys in a nice string.
-  puts "#{student[:name]}".ljust(20) + "(#{student[:cohort]} cohort)".ljust(20)
 
-  end  
-  end  
+  def show_students
+    print_header
+    print_students_list
+    print_footer
   end
-end
+  
+  def print_header
+    puts "The students of Villains Academy"
+    puts "_____________"
+  end
+  
+  def print_students_list
+    print_count = 1
+    while print_count <= 1 do
+      print_count += 1
+      cohorts = @students.map { |p| p[:cohort] }.uniq
+      #maps the "students" array - each element a hash -, designates each hash 'p', 
+      #takes each hash and calls the value of the cohort key 
+      # .uniqs them so there aren't doubles and puts them in an array called "cohorts". So an order is set.
+      cohorts.each do |x|
+        # goes through the new array "cohorts" and designates each month as X  
+      temp_students = @students.select { |p| p[:cohort] == x}
+      # creates a new array "temp_students" then selects to go in it from 
+      #the "students" array the hashes where cohort equals eg november, december then jan as per order from .uniq
+      temp_students.each do |student|
+        #temp_students is now an array filled with elements that are hashes 
+        #in order of month. this designates each hash as "student" 
+      # puts "#{student[:name]}".ljust(20) + "Hobby: #{student[:hobbies]}".ljust(25) + "Country of Birth: #{student[:country_of_birth]}".ljust(35) + "Height: #{student[:height]}".ljust(25) + "(#{student[:cohort]} cohort)".ljust(20)
+    #then says print each "student" with these keys in a nice string.
+    puts "#{student[:name]}".ljust(20) + "(#{student[:cohort]} cohort)".ljust(20)
+    end  
+    end  
+    end
+  end
+  
+  def print_footer
+    puts "Overall, we have #{@students.count} great students"
+  end
 
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "_____________"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
 
 def save_students
   file = File.open("students.csv", "w")
@@ -112,8 +112,8 @@ end
 file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -121,4 +121,18 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, {filename} doesn't exist."
+    exit
+  end
+  end
+
+
+try_load_students
 interactive_menu
